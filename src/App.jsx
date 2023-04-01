95050915c222e5f557ca166acdd789e94e0cacff
Ricky. D. Rodrigues <93785760+rickyrodrigues@users.noreply.github.com>
2023-04-01 07:55:50 +0000

    deploy to github

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Ricky rodrigues",
+  title: "Web Designer & Content Creator,alpha msla ",
+  email: "rickyrodrigz1762003@gmail.com",
+  gitHub: "https://github.com/rickyrodrigues",
+  instagram: "https://www.instagram.com/ricky_rodrigz/",
+  linkedIn: "https://www.linkedin.com/in/ricky-rodrigues-97b305201/",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "-",
+  youTube: "-",
 };
 
 const primaryColor = "#4E567E";
